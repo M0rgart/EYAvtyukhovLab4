9646da4677183a3a003b6d3beaa7ac3a4392cd18
Егор <egor.avtyuhov@yandex.ru>
2025-12-07 22:21:24 +0300

    added separator in the main

--- src/main.py
+++ src/main.py
@@ -2,17 +2,25 @@ import simulation
 
 
 def main() -> None:
+    sep = '\n' + '=' * 50 + '\n'
+
     print('Симуляция №1')
     print('Вызов симуляции без указания переменных')
     simulation.run_sim()
 
-    print('\nСимуляция №2')
+    print(sep)
+
+    print('Симуляция №2')
     print('Вызов симуляции из 10 шагов')
     simulation.run_sim(steps=10)
+
+    print(sep)
 
     print('\nСимуляция №3')
     print('Вызов симуляции по сиду')
     simulation.run_sim(seed=67)
+
+    print(sep)
 
     print('\nСимуляция №4')
     print('Вызов симуляции по сиду с ограниченным количеством шагов')
