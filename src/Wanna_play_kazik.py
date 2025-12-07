4abd98a935abe2c84e69be4ca5aae1290f752bbf
Егор <egor.avtyuhov@yandex.ru>
2025-12-08 00:53:49 +0300

    small changes (blance -> balances

--- src/Wanna_play_kazik.py
+++ src/Wanna_play_kazik.py
@@ -13,39 +13,39 @@ class Chip:
         return f'Chip(value={self.value})'
 
 
-class Casino_balance(MutableMapping):
+class CasinoBalance(MutableMapping):
     def __init__(self):
-        self._balance = Dict[str, int] = {}
+        self._balances: Dict[str, int] = {}
         self._change_log: List[str] = []
 
     def __getitem__(self, key: str) -> int:
-        return self._balance[key]
+        return self._balances[key]
 
     def __setitem__(self, key: str, value: int) -> None:
-        old_value = self._balance.get(key, 0)
-        self._balance[key] = value
+        old_value = self._balances.get(key, 0)
+        self._balances[key] = value
         change = value - old_value
         log = f'Балланс {key}: {old_value} -> {value} (Изменение: {'+' if change >+ 0 else ''}{change})'
         self._change_log.append(log)
         print(f"[LOG] {log}")
 
     def __delitem__(self, key: str) -> None:
-        del self._balance[key]
+        del self._balances[key]
 
     def __iter__(self) -> Iterator[str]:
-        return iter(self._balance)
+        return iter(self._balances)
 
     def __len__(self) -> int:
-        return len(self._balance)
+        return len(self._balances)
 
     def __contains__(self, key: str) -> bool:
-        return key in self._balance
+        return key in self._balances
 
     def get_log(self) -> List[str]:
         return self._change_log
 
     def __repr__(self) -> str:
-        return f'Casino_balance({self._balance})'
+        return f'Casino_balance({self._balances})'
 
 
 class Casino:
